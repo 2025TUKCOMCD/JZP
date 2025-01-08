986db176d7e4cc8b0aecb6686afcc2f6c3d37011
minjaon <minjaon@naver.com>
2025-01-08 15:06:55 +0900

    fix:BE 개발환경

--- JZP_BE/jzp/src/main/java/com/example/jzp/hello.java
+++ JZP_BE/jzp/src/main/java/com/example/jzp/hello.java
@@ -1,13 +1,14 @@
 package com.example.jzp;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class hello {
-    
-    @RequestMapping("/")
-    public String index() {
-        return "Hello, Spring Boot!";
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
     }
 }
