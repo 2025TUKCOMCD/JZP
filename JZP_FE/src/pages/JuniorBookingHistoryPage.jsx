d652a0a46474d8c15764128253d5b1a29d0780f3
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-01-15 05:32:57 +0900

    feat : page routing

---
+++ JZP_FE/src/pages/JuniorBookingHistoryPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/header.jsx";
+import StepBar from "../components/movieStepBar.jsx";
+
+function JuniorBookingHistoryPage() {
+  return (
+    <div className="bg-customBg h-screen text-white flex flex-col">
+      <Header />
+      <StepBar />
+    </div>
+  );
+}
+
+export default JuniorBookingHistoryPage;
---
+++ JZP_FE/src/pages/SeniorSeatSelectPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header.jsx";
+
+function SeniorSeatSelectPage() {
+  return (
+    <div className="bg-customBg h-screen text-white flex flex-col">
+      <Header />
+    </div>
+  );
+}
+
+export default SeniorSeatSelectPage;
