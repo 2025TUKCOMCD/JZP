d247f7735d48d93bc0b01842cd61e734a18e1b4a
seung yeon <137742302+moanuna@users.noreply.github.com>
2025-01-31 13:50:19 +0900

    feat : 예매 내역 전송하기 페이지 기본 틀

---
+++ JZP_FE/src/pages/JuniorSendingPage.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/header.jsx";
+
+function JuniorSendingPage() {
+  return (
+    <div className="bg-customBg h-screen text-white flex flex-col">
+      <Header />
+    </div>
+  );
+}
+
+export default JuniorSendingPage;
